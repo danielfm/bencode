1a68eda830fed5c4134ea04cbd1f713c998a8dd4
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-03-31 10:12:14 -0300

    Changed indentation to make the code more readable.

--- test/bencode/decoder_test.clj
+++ test/bencode/decoder_test.clj
@@ -4,75 +4,97 @@
 
 (deftest string-decoding
   (testing "Decoding a non-empty string"
-    (is (= "spam" (bdecode "4:spam"))))
+    (is (= "spam"
+           (bdecode "4:spam"))))
 
   (testing "Decoding an empty string"
-    (is (= "" (bdecode "0:"))))
+    (is (= ""
+           (bdecode "0:"))))
 
   (testing "Decoding a string that is shorter than expected"
-    (is (thrown? IllegalArgumentException (bdecode "4:spa"))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "4:spa"))))
 
   (testing "Decoding a non-empty string with garbage at the end"
-    (is (thrown? IllegalArgumentException (bdecode "4:spam0:")))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "4:spam0:")))))
 
 (deftest number-encoding
   (testing "Decoding a positive number"
-    (is (= 64 (bdecode "i64e"))))
+    (is (= 64
+           (bdecode "i64e"))))
 
   (testing "Decoding a positive number with sign"
-    (is (= 64 (bdecode "i+64e"))))
+    (is (= 64
+           (bdecode "i+64e"))))
 
   (testing "Decoding a negative number"
-    (is (= -32 (bdecode "i-32e"))))
+    (is (= -32
+           (bdecode "i-32e"))))
 
   (testing "Decoding zero"
-    (is (zero? (bdecode "i0e"))))
+    (is (zero?
+         (bdecode "i0e"))))
 
   (testing "Decoding empty number"
-    (is (thrown? IllegalArgumentException (bdecode "ie"))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "ie"))))
 
   (testing "Decoding empty number with sign"
-    (are [s] (thrown? IllegalArgumentException (bdecode s))
+    (are [s] (thrown? IllegalArgumentException
+                      (bdecode s))
          "i-e" "i+e"))
 
   (testing "Decoding a number with more than one sign"
-    (is (thrown? IllegalArgumentException (bdecode "i+-10e"))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "i+-10e"))))
 
   (testing "Decoding invalid 'negative zero'"
-    (is (thrown? IllegalArgumentException (bdecode "i-0e"))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "i-0e"))))
 
   (testing "Decoding multiple numbers inside the same unit"
-    (is (thrown? IllegalArgumentException (bdecode "i30-20e")))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "i30-20e")))))
 
 (deftest sequence-decoding
   (testing "Decoding an empty sequence"
-    (is (= [] (bdecode "le"))))
+    (is (= []
+           (bdecode "le"))))
 
   (testing "Decoding a simple non-empty sequence"
-    (is (= ["spam" "eggs" 64] (bdecode "l4:spam4:eggsi64ee"))))
+    (is (= ["spam" "eggs" 64]
+           (bdecode "l4:spam4:eggsi64ee"))))
 
   (testing "Decoding nested sequences"
-    (is (= ["spam" ["eggs" 64]] (bdecode "l4:spaml4:eggsi64eee")))))
+    (is (= ["spam" ["eggs" 64]]
+           (bdecode "l4:spaml4:eggsi64eee")))))
 
 (deftest dict-decoding
   (testing "Use a sorted map"
     (is (sorted? (bdecode "de"))))
 
   (testing "Decoding an empty dictionary"
-    (is (= {} (bdecode "de"))))
+    (is (= {}
+           (bdecode "de"))))
 
   (testing "Decoding a simple non-empty dictionary"
-    (is (= {:spam 64 :cow "moo"} (bdecode "d3:cow3:moo4:spami64ee"))))
+    (is (= {:spam 64 :cow "moo"}
+           (bdecode "d3:cow3:moo4:spami64ee"))))
 
   (testing "Decoding a dictionary containing a sequence"
-    (is (= {:spam ["eggs" 64]} (bdecode "d4:spaml4:eggsi64eee"))))
+    (is (= {:spam ["eggs" 64]}
+           (bdecode "d4:spaml4:eggsi64eee"))))
 
   (testing "Decoding nested dictionaries"
-    (is (= {:spam {:cow "moo"}} (bdecode "d4:spamd3:cow3:mooee"))))
+    (is (= {:spam {:cow "moo"}}
+           (bdecode "d4:spamd3:cow3:mooee"))))
 
   (testing "Decoding a dictionary with invalid keys"
-    (is (thrown? IllegalArgumentException (bdecode "di64e3:cowe")))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "di64e3:cowe")))))
 
 (deftest unsupported-decoding
   (testing "Decoding unknown type"
-    (is (thrown? IllegalArgumentException (bdecode "x10")))))
+    (is (thrown? IllegalArgumentException
+                 (bdecode "x10")))))
--- test/bencode/encoder_test.clj
+++ test/bencode/encoder_test.clj
@@ -4,34 +4,43 @@
 
 (deftest string-encoding
   (testing "Encoding a non-empty string"
-    (is (= "4:spam" (bencode "spam"))))
+    (is (= "4:spam"
+           (bencode "spam"))))
 
   (testing "Encoding an empty string"
-    (is (= "0:" (bencode "")))))
+    (is (= "0:"
+           (bencode "")))))
 
 (deftest keyword-encoding
   (testing "Encoding a keyword"
-    (is (= "4:spam" (bencode :spam)))))
+    (is (= "4:spam"
+           (bencode :spam)))))
 
 (deftest number-encoding
   (testing "Encoding a positive number"
-    (is (= "i64e" (bencode 64))))
+    (is (= "i64e"
+           (bencode 64))))
 
   (testing "Encoding a negative number"
-    (is (= "i-32e" (bencode -32))))
+    (is (= "i-32e"
+           (bencode -32))))
 
   (testing "Encoding zero"
-    (is (= "i0e" (bencode 0)))))
+    (is (= "i0e"
+           (bencode 0)))))
 
 (deftest sequence-encoding
   (testing "Encoding an empty sequence"
-    (is (= "le" (bencode []))))
+    (is (= "le"
+           (bencode []))))
 
   (testing "Encoding a simple non-empty sequence"
-    (is (= "l4:spam4:eggsi64ee" (bencode (seq ["spam" "eggs" 64])))))
+    (is (= "l4:spam4:eggsi64ee"
+           (bencode (seq ["spam" "eggs" 64])))))
 
   (testing "Encoding nested sequences"
-    (is (= "l4:spaml4:eggsi64eee" (bencode #{"spam" ["eggs" 64]})))))
+    (is (= "l4:spaml4:eggsi64eee"
+           (bencode #{"spam" ["eggs" 64]})))))
 
 (deftest dict-encoding
   (testing "Encoding an empty dictionary"
@@ -44,11 +53,12 @@
     (is (= "d4:spaml4:eggsi64eee" (bencode {:spam ["eggs" 64]}))))
 
   (testing "Encoding nested dictionaries"
-    (is (= "d4:spamd3:cow3:mooee" (bencode {:spam {:cow "moo"}}))))
+    (is (= "d4:spamd3:cow3:mooee"
+           (bencode {:spam {:cow "moo"}}))))
 
   (testing "Encoding a dictionary with invalid keys"
     (are [key] (thrown? IllegalArgumentException (bencode {key "val"}))
-         "key" [] {} 64)))
+         [] {} 64)))
 
 (deftest unsupported-encoding
   (testing "Encoding unsupported data types"
