490020507eeefca9405b439b36b7a4122ea538fc
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-04-27 09:46:05 -0300

    New version.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject bencode "0.2.0"
+(defproject bencode "0.2.1"
   :description "BitTorrent encoding implementation for Clojure."
   :url "https://github.com/danielfm/bencode"
   :license {:name "BSD License"
             :url "http://raw.github.com/danielfm/bencode/master/COPYING"}
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
   :test-paths ["src/test/clojure"])
