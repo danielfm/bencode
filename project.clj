1f1577038b36c45df5347cb49d2e8ade86ba5fa5
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-04-28 12:21:50 -0300

    Incremented version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bencode "0.2.3"
+(defproject bencode "0.2.4"
   :description "BitTorrent encoding implementation for Clojure."
   :url "https://github.com/danielfm/bencode"
   :license {:name "BSD License"
